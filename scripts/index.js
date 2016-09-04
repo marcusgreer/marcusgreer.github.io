2f52445f401bd7b485a5b46d917e9112c556eb3c
marcusgreer96 <mgg787@gmail.com>
2016-09-04 17:09:41 -0400

    bugs fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,8 @@
-// $(document).ready(function () {
+$(document).ready(function () {
     $(document).on("mouseenter","div.container", function() {
-        $('div').remove('.waves');
-        $("div.container").append("\
-        <div class='clouds'>\
-        <nav>\
-            <!-- <div class='nav_wraper'> -->\
-                <ul>\
-                    <li><a href='/'>Home</a></li>\
-                    <li><a href='/Marcus_Greer_Resume.pdf'>About</a></li>\
-                    <li><a href='/projects'>Projects</a></li>\
-                    <li><a href='/Marcus_Greer_Resume.pdf'>Resume</a></li>\
-                    <li><a href='/blog'>Raps</a></li>\
-                </ul>\
-            <!-- </div> -->\
-        </nav>\
-        </div>");
+        $('div.waves').fadeOut();
     });
     $(document).on("mouseleave","div.container", function() {
-        $('div').remove('.clouds');
-        $("div.container").append("\
-            <div class='waves'>\
-                <canvas id='waves'></canvas>\
-                <script src='scripts/waves.js'></script>\
-            </div>");
+        $('div.waves').fadeIn();
     });
-// });
+});
